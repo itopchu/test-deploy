02f1b16f094ba25861d471e59cddef57ab7d4325
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-03 13:15:57 +0300

    Add camera button

--- public/pwa_zoo/src/js/QRrecognizer.js
+++ public/pwa_zoo/src/js/QRrecognizer.js
@@ -1,21 +1,26 @@
 const video = document.createElement("video");
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
+const qrButton = document.querySelector(".qrImage");
 
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then((stream) => {
-        video.srcObject = stream;
-        document.body.appendChild(video);
-        return video.play();
-    })
-    .then(() => {
-        // Set up a loop to continuously capture frames
-        requestAnimationFrame(processVideo);
-    })
-    .catch((error) => {
-        console.error("Error accessing the camera: ", error);
-    });
+// Function to start video capture
+function startVideoCapture() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            video.srcObject = stream;
+            document.body.appendChild(video);
+            return video.play();
+        })
+        .then(() => {
+            // Set up a loop to continuously capture frames
+            requestAnimationFrame(processVideo);
+        })
+        .catch((error) => {
+            console.error("Error accessing the camera: ", error);
+        });
+}
 
+// Function to process video frames
 function processVideo() {
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
@@ -41,8 +46,8 @@ function processVideo() {
     requestAnimationFrame(processVideo);
 }
 
+// Function to match QR codes with predefined data
 function matchQRCode(data) {
-    // Replace this with your pool of QR codes and corresponding image sources
     const qrCodePool = {
         "https://omerakgull.github.io/pwa_zoo/harita/harita.html": { url: "https://omerakgull.github.io/pwa_zoo/harita/harita.html", imageSrc: "img/qrcodes/map.png" },
         // Add more QR codes and image sources as needed
@@ -59,3 +64,6 @@ window.addEventListener("beforeunload", () => {
         tracks.forEach(track => track.stop());
     }
 });
+
+// open camera on click
+qrButton.addEventListener("click", startVideoCapture);
