2f4d8058d231e4e31bd2b5ccf1d9f03cbff9752e
Am0rA <i.topcu1234567890@gmail.com>
2024-01-03 15:18:20 +0100

    test update on middlewares.js

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,31 +1,32 @@
 module.exports = [
-    'strapi::logger',
-    'strapi::errors',
-    {
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-              'media-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'market-assets.strapi.io',
-                'res.cloudinary.com',
-              ],
-              upgradeInsecureRequests: null,
-            },
+  'strapi::logger',
+  'strapi::errors',
+  {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              'res.cloudinary.com',
+            ],
+            'script-src': ["'self'", 'cdn.jsdelivr.net', 'https:'],  // Add 'cdn.jsdelivr.net' to allow scripts from this domain
+            upgradeInsecureRequests: null,
           },
         },
-      },,
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+      },
+    },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
